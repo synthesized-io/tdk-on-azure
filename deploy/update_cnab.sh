1971ef09bd956213fc3aaf5dd56766ff6ec6fe13
Vitaly Ivanov <vitaly@synthesized.io>
2024-02-06 16:53:41 +0000

    Automate release process for Azure Marketplace (#3)

---
+++ deploy/update_cnab.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -u
+
+: "${ACCESS_TOKEN:?Need to set ACCESS_TOKEN}"
+: "${TENANT_ID:?Need to set TENANT_ID}"
+: "${SUBSCRIPTION_ID:?Need to set SUBSCRIPTION_ID}"
+: "${RESOURCE_GROUP:?Need to set RESOURCE_GROUP}"
+: "${PRODUCT_ID:?Need to set PRODUCT_ID}"
+: "${PLAN_ID:?Need to set PLAN_ID}"
+: "${PLAN_NAME:?Need to set PLAN_NAME}"
+: "${REGISTRY_NAME:?Need to set REGISTRY_NAME}"
+: "${REPOSITORY_NAME:?Need to set REPOSITORY_NAME}"
+: "${CNAB_TAG:?Need to set CNAB_TAG}"
+: "${CNAB_DIGEST:?Need to set CNAB_DIGEST}"
+
+PLAN=$(curl -X GET "https://graph.microsoft.com/rp/product-ingestion/container-plan-technical-configuration/$PRODUCT_ID/$PLAN_ID" \
+-H "Authorization: Bearer $ACCESS_TOKEN" | jq)
+
+PLAN=$(echo "$PLAN" | jq '.cnabReferences += [{
+  "tenantId": "'"$TENANT_ID"'",
+  "subscriptionId": "'"$SUBSCRIPTION_ID"'",
+  "resourceGroupName": "'"$RESOURCE_GROUP"'",
+  "registryName": "'"$REGISTRY_NAME"'",
+  "repositoryName": "'"$REPOSITORY_NAME"'",
+  "tag": "'"$CNAB_TAG"'",
+  "digest": '"$CNAB_DIGEST"'
+}]')
+
+curl -X POST 'https://graph.microsoft.com/rp/product-ingestion/configure?$version=2022-03-01-preview2' \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $ACCESS_TOKEN" \
+-d '{
+  "$schema": "https://schema.mp.microsoft.com/schema/configure/2022-03-01-preview2",
+  "resources": [
+    '"$PLAN"'
+  ]
+}'
---
+++ deploy/wait_for_request_result.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function wait_for_result() {
+    job_id=$1
+    access_token=$2
+
+    for i in {1..15}; do
+        response=$(curl -X GET 'https://graph.microsoft.com/rp/product-ingestion/configure/'"$job_id"'/status?$version=2022-03-01-preview2' -H "Authorization: Bearer $access_token" | jq)
+        echo "Response $response"
+        response_status=$(jq -r '.jobResult' <<< "$response")
+        if [[ ${response_status} == 'failed' ]]; then
+          echo "Failed response $response"
+          return 1
+        fi
+        if [[ ${response_status} == 'succeeded' ]]; then
+          echo "Success response $response"
+          return 0
+        else
+          echo "Attempt $i/15. Intermediate response status $response_status"
+        fi
+        sleep 5
+    done
+
+    echo "Retry limit exceeded"
+    return 1
+}
